60c2fac6898108c58583f088cceae7559804db05
Maciej Nowakowski <s27023@pjwstk.edu.pl>
2026-05-20 11:01:00 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+﻿import sys
+from server import main as server_main
+from client import main as client_main
+
+if __name__ == '__main__':
+    if "server" in sys.argv:
+        server_main()
+    if "client" in sys.argv:
+        client_main()
