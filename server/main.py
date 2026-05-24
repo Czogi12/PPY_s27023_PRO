4fbc0bbfa5c19912c8ec88f540f46ce816dd7d0e
Maciej Nowakowski <s27023@pjwstk.edu.pl>
2026-05-24 16:14:34 +0200

    added basic user router

--- server/main.py
+++ server/main.py
@@ -1,17 +1,20 @@
-﻿import uvicorn
+﻿import socketio
+import uvicorn
 from fastapi import FastAPI
+from .routers.user_router import router as user_router
 
-app = FastAPI()
+fastapi_app = FastAPI()
+sio = socketio.AsyncServer(async_mode="asgi", cors_allowed_origins="*")
 
-print("Running server")
+fastapi_app.include_router(user_router)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@sio.event
+async def connect(sid, environ):
+    print("client connected", sid)
 
+@sio.event
+async def message(sid, data):
+    await sio.emit("reply", {"echo": data}, to=sid)
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
-
+app = socketio.ASGIApp(sio, other_asgi_app=fastapi_app)
 uvicorn.run(app, host="127.0.0.1", port=8000)
---
+++ server/routers/user_router.py
@@ -0,0 +1,9 @@
+﻿from fastapi import APIRouter
+from ..services.user_service import UserService
+
+router = APIRouter(prefix="/users")
+service = UserService()
+
+@router.get("/{user_id}")
+def get_user(user_id: int):
+    return service.get_user(user_id)
